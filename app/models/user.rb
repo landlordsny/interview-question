df0e90e0326922b3b906950bb9ab19947d147c79
Andrei Motinga <andrew.motinga@gmail.com>
2015-12-24 11:15:27 -0500

    allows to select users with read message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :messages
+
+  def self.with_read_message
+    all.select(&:message?)
+  end
+
+  def message?
+    messages.where('read_at < ?', Time.current).any?
+  end
 end
